03337c78b411b8fd36a063c44b0995d4c79c53c6
Oleksandr Zhurba <starkyzh@gmail.com>
2023-06-20 19:51:55 +0200

    feat: add class Constants.java for global constants

---
+++ src/Constants.java
@@ -0,0 +1,10 @@
+import java.text.SimpleDateFormat;
+
+public class Constants {
+  public static final SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+  public final static String SEP = ";";
+  public final static String SEP_PLACES = "\\|";
+  public static final String FILMS_FILENAME = "res/FilmList.csv";
+  public static final String SESSIONS_FILENAME = "res/SessionList.csv";
+
+}
