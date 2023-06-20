82a90b4b8fdf3d47715d287b2cec6c6f183feb1b
Oleksandr Zhurba <starkyzh@gmail.com>
2023-06-20 19:53:33 +0200

    feat: add methods to work with places in a session, override toString

--- src/Session.java
+++ src/Session.java
@@ -1,26 +1,16 @@
 import java.util.Date;
-import java.util.Map;
+import java.util.List;
 
 public class Session {
 
-  private String timeSession;
   private Film film;
   private Date dateStart;
-  private Map<String, Boolean> places;
+  private List<List<Integer>> places;
 
-  public Session(String timeSession, Film film, Date dateStart, Map<String, Boolean> places) {
-    this.timeSession = timeSession;
+  public Session(Film film, Date dateStart, List<List<Integer>> places) {
     this.film = film;
     this.dateStart = dateStart;
     this.places = places;
-  }
-
-  public String getTimeSession() {
-    return timeSession;
-  }
-
-  public void setTimeSession(String timeSession) {
-    this.timeSession = timeSession;
   }
 
   public Film getFilm() {
@@ -39,11 +29,75 @@ public class Session {
     this.dateStart = dateStart;
   }
 
-  public Map<String, Boolean> getPlaces() {
+  public List<List<Integer>> getPlaces() {
     return places;
   }
 
-  public void setPlaces(Map<String, Boolean> places) {
+  public void setPlaces(List<List<Integer>> places) {
     this.places = places;
+  }
+
+  @Override
+  public String toString() {
+    return "Session{" +
+        ", film=" + film +
+        ", dateStart=" + dateStart +
+        ", places=" + places +
+        '}';
+  }
+
+  public String toFile() {
+    return film + Constants.SEP +
+        Constants.formatter.format(dateStart) + Constants.SEP +
+        placesToText(places);
+  }
+
+  private static String placesToText(List<List<Integer>> places) {
+    String result = "";
+    for (int i = 0; i < places.size(); i++) {
+      List<Integer> row = places.get(i);
+      for (int j = 0; j < row.size(); j++) {
+        result += row.get(j);
+      }
+      result += Constants.SEP_PLACES;
+    }
+    result = result.substring(0, result.length() - 1);
+    return result;
+  }
+
+  public void printMapPlaces() {
+    System.out.println();
+
+    System.out.println("Карта свободных и занятых мест на сеанс:");
+    System.out.println(film.toStringPretty());
+    System.out.println("Дата сеанса: " + Constants.formatter.format(dateStart));
+
+    int rowSize = 0;
+    if (places.size() > 0) {
+      rowSize = places.get(0).size();
+    }
+    System.out.print("     " + ":\t");
+    for (
+        int i = 0;
+        i < rowSize; i++) {
+      System.out.print((i + 1) + " \t");
+    }
+    System.out.println();
+    for (
+        int i = 0; i < places.size(); i++) {
+      System.out.print("Ряд " + (i + 1) + ": \t");
+      List<Integer> row = places.get(i);
+      for (int j = 0; j < row.size(); j++) {
+        int free = row.get(j);
+        if (free == 0) {
+          System.out.print("R");
+        } else {
+          System.out.print("F");
+        }
+        System.out.print(" \t");
+      }
+      System.out.println();
+    }
+    System.out.println("+++++++++++++++++++++++++++++++++++++");
   }
 }
