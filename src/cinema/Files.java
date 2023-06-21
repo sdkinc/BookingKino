e14314677850c4f44a8b172700cbc623fac5f879
Oleksandr Zhurba <starkyzh@gmail.com>
2023-06-21 20:06:53 +0200

    feat: add comments

--- src/cinema/Files.java
+++ src/cinema/Files.java
@@ -10,7 +10,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Files {
-
+  /***
+   * Метод получает на вход объект типа Film и дописывает его в файл.
+   * @param film
+   * @throws IOException
+   */
   static void writeObjectToFile(Film film) throws IOException {
     List<Film> listFilmFromFile = readFilmsFromFile();
     FileWriter fileWriter = new FileWriter(Constants.FILMS_FILENAME);
@@ -20,7 +24,11 @@ public class Files {
     fileWriter.write(film.toFile());
     fileWriter.close();
   }
-
+  /***
+   * Метод получает на вход объект типа Session и дописывает его в файл.
+   * @param session
+   * @throws IOException
+   */
   static void writeObjectToFile(Session session) throws IOException, ParseException {
     List<Session> listFromFile = readSessionsFromFile();
     FileWriter fileWriter = new FileWriter(Constants.SESSIONS_FILENAME);
@@ -31,6 +39,12 @@ public class Files {
     fileWriter.close();
   }
 
+  /***
+   * Функция читает сеансы из файла в объект типа List<Session>
+   * @return возвращает List<Session>
+   * @throws IOException
+   * @throws ParseException
+   */
   public static List<Session> readSessionsFromFile() throws IOException, ParseException {
     Scanner scanner = new Scanner(new FileReader(Constants.SESSIONS_FILENAME));
     checkExistFile(Constants.SESSIONS_FILENAME);
@@ -41,6 +55,11 @@ public class Files {
     return result;
   }
 
+  /***
+   * Функция читает фильмы из файла в объект типа List<Film>
+   * @return возвращает List<Film>
+   * @throws IOException
+   */
   static List<Film> readFilmsFromFile() throws IOException {
     Scanner scanner = new Scanner(new FileReader(Constants.FILMS_FILENAME));
     checkExistFile(Constants.FILMS_FILENAME);
@@ -51,6 +70,11 @@ public class Files {
     return result;
   }
 
+  /***
+   * Метод проверяет существует ли файл, и если не существует, то создает новый и записывает его.
+   * @param filename
+   * @throws IOException
+   */
   private static void checkExistFile(String filename) throws IOException {
     File file = new File(filename);
     if (!file.exists()) {
@@ -59,6 +83,11 @@ public class Files {
     }
   }
 
+  /***
+   * Функция читает фильмы из файла в объект типа ArrayList<Hall>
+   * @return объект ArrayList<Hall>
+   * @throws IOException
+   */
   static ArrayList<Hall> readHallFromFile() throws IOException {
     String filename = "res/HallList.csv";
     Scanner scanner = new Scanner(new FileReader(filename));
@@ -70,6 +99,11 @@ public class Files {
     return result;
   }
 
+  /***
+   * Функция создает кинотеатр читая из файла список залов и список фильмов
+   * @return объект типа Cinema
+   * @throws IOException
+   */
   public static Cinema readCinemaFromFile() throws IOException {
     return new Cinema(Constants.NAME_CINEMA, Files.readHallFromFile(),
         Files.readFilmsFromFile());
