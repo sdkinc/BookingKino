dfabed7e9aad852b7e590bffe09e55a8ea754320
Oleksandr Zhurba <starkyzh@gmail.com>
2023-06-21 20:07:07 +0200

    fix: style code

--- src/tests/HallTests.java
+++ src/tests/HallTests.java
@@ -2,33 +2,33 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import cinema.Constants;
 import cinema.Hall;
 import org.junit.jupiter.api.Test;
 
 public class HallTests {
+
   @Test
-  public void testConstructor(){
+  public void testConstructor() {
     //arrange
     String name = "RED";
-    int price  =50;
+    int price = 50;
 
     //act
-    Hall hall = new Hall(name,price);
-    assertEquals(name,hall.getName());
-    assertEquals(price,hall.getPrice());
+    Hall hall = new Hall(name, price);
+    assertEquals(name, hall.getName());
+    assertEquals(price, hall.getPrice());
   }
 
   @Test
-  public void testParseFromString(){
+  public void testParseFromString() {
     //arrange
     String string = "RED;50";
     String name = "RED";
-    int price  =50;
+    int price = 50;
 
     //act
     Hall hall = Hall.parseHallFromString(string);
-    assertEquals(name,hall.getName());
-    assertEquals(price,hall.getPrice());
+    assertEquals(name, hall.getName());
+    assertEquals(price, hall.getPrice());
   }
 }
