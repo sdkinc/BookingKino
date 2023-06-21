9f71eb76db85f71c08460594bf2f4567831cf71d
Oleksandr Zhurba <starkyzh@gmail.com>
2023-06-21 20:06:39 +0200

    feat: add comments

--- src/cinema/Constants.java
+++ src/cinema/Constants.java
@@ -3,12 +3,18 @@ package cinema;
 import java.text.SimpleDateFormat;
 
 public class Constants {
-
+  //formatter используется в разных классах, для приведения даты к единому формату
+  // при чтении и записи в файл из файла
   public static final SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+  //SEP разделитель колонок в строках с данными в файлах
   public final static String SEP = ";";
+  //SEP_PLACES разделитель рядов в поле с данными по местам в файле
   public final static String SEP_PLACES = "\\|";
+  //FILMS_FILENAME путь к файлу с фильмами
   public static final String FILMS_FILENAME = "res/FilmList.csv";
+  //SESSIONS_FILENAME путь к файлу с сеансами
   public static final String SESSIONS_FILENAME = "res/SessionList.csv";
   public static Cinema cinema = null;
+  //NAME_CINEMA наименование кинотеатра
   public static final String NAME_CINEMA = "Cinemax AIT entertainment";
 }
